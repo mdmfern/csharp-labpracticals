ee5c994f4eecf4d0065acf1a26611cb66c281de3
Devin Fernando <136495746+mdmfern@users.noreply.github.com>
2023-07-30 21:39:49 +0530

    Add files via upload

---
+++ lab4/lab4/ConvertValues.cs
@@ -0,0 +1,6 @@
+﻿namespace lab4
+{
+    internal class ConvertValues
+    {
+    }
+}
---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertValues convert = new ConvertValues();
+
+            Console.WriteLine("Enter Your distance in kilometer: ");
+            float km = (float)Convert.ToDecimal(Console.ReadLine());
+
+            float meter = convert.KilometerTometer(float km);
+            Console.WriteLine("Your distance in Kilometer is: " + meter);
+
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object creation
+            FindValues values = new FindValues();
+
+            //Getting radius from user
+            Console.WriteLine("Enter your circles radius: ");
+            float r = (float)Convert.ToDecimal(Console.ReadLine());
+
+            float area = values.FindArea(r);
+            float circum = values.FindCircumference(r);
+
+            Console.WriteLine("Your circle's area is: " + area);
+            Console.WriteLine("Your circle's circumference is: " + circum);
+
+            Console.ReadKey();
+        }
+    }
+}
